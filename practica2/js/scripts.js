ac8e1a9b21183f45ee39eb8393a6f50a8129a299
Jesus Rios <jesusriosdev@gmail.com>
2020-10-06 02:59:25 -0700

    archivos pendientes

---
+++ practica2/js/scripts.js
@@ -0,0 +1,10 @@
+$(window).on("load", function(){
+    // Handler when all assets (including images) are loaded.
+
+    // Find carousel.
+    // document.getElementById("carouselExampleIndicators").carousel({
+    //     interval: 100
+    // });
+    
+    $('#mycarousel').carousel({ interval: 2000 });
+  });
