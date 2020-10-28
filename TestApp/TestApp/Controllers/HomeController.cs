f292c116f26d84a32be8d6c0afce358344b7f888
Jesus Rios <jesusriosdev@gmail.com>
2020-10-28 11:29:02 -0700

    proyecto 26 de oct

---
+++ TestApp/TestApp/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TestApp.Models;
+
+namespace TestApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult TestView()
+        {
+            var model = new UserViewModel();
+            model.FirstName = "Jesus";
+            model.LastName = "Rios";
+            model.Age = 29;
+
+            //ViewBag.TestName = "Jesus Rios";
+
+            return View(model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
